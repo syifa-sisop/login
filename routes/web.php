be4f125e7137fa92898f9ebb50a588d3e7796327
Syifa3112 <58512859+syifa-sisop@users.noreply.github.com>
2023-05-25 20:28:18 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\DataSiswaController;
 use App\Http\Controllers\KelasController;
 use App\Http\Controllers\AbsensiController;
 use App\Http\Controllers\LayoutController;
+use App\Http\Controllers\SiswaKelasController;
 
 /*
 |--------------------------------------------------------------------------
@@ -41,20 +42,20 @@ Route::group(['middleware' => ['auth']], function(){
         Route::resource('kelas',KelasController::class);
         Route::resource('datasiswa',DataSiswaController::class);
         Route::resource('absensi',AbsensiController::class);
+
+        Route::delete('/delete/{id}',[KelasController::class, 'delete'])->name('delete');
+
         Route::patch('/update/{id}', ['as' => 'datasiswa.update', 'uses' => 'App\Http\Controllers\DataSiswaController@update']);
         Route::delete('/destroy/{id}', ['as' => 'datasiswa.destroy', 'uses' => 'App\Http\Controllers\DataSiswaController@destroy']);
 
 
-        Route::patch('/update/{id}', ['as' => 'dataguru.update', 'uses' => 'App\Http\Controllers\DataGuruController@update']);
-        Route::delete('/delete/{id}', ['as' => 'dataguru.delete', 'uses' => 'App\Http\Controllers\DataGuruController@delete']);
-
-        Route::post('/kelas/', 'App\Http\Controllers\KelasController@create');
-        Route::put('/kelas/', 'App\Http\Controllers\KelasController@update');
-        //Route::delete('/delete/{id}', 'App\Http\Controllers\KelasController@delete');
-
-        Route::delete('/delete/{id}', ['as' => 'kelas.delete', 'uses' => 'App\Http\Controllers\KelasController@delete']);
-
         Route::get('datasiswa', [App\Http\Controllers\DataSiswaController::class,'index'])->name('datasiswa.search');
+
+
+        Route::post('/siswakelas/', 'App\Http\Controllers\SiswaKelasController@create');
+        Route::get('/siswakelas/{id}', 'App\Http\Controllers\SiswaKelasController@add');
+        Route::delete('/siswakelas/delete/{id}', 'App\Http\Controllers\SiswaKelasController@delete');
+
         
         
     });
