e76e70c9df980b6959609ceba36eef7c4084396c
Syifa3112 <58512859+syifa-sisop@users.noreply.github.com>
2023-05-29 21:22:16 +0700

    perbaikan absensi

--- app/Http/Controllers/AbsensiController.php
+++ app/Http/Controllers/AbsensiController.php
@@ -7,40 +7,31 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\KelasSiswa;
 use App\Models\Absensi;
 use App\Models\Kelas;
-use Carbon\Carbon;
-use DB;
+
 
 class AbsensiController extends Controller
 {
-     public function index(){
+     public function index()
+     {
+
         $kelas2     = Kelas::paginate(5);
         $this->model = new Absensi;
         $data = $this->model->tampil_absen();
 
         return view('admin/listkelas', ['kelas2'=>$kelas2, 'user' => Auth::user(), 'data'=> $data]);
     }
-    public function show($id){
+
+    public function show($id)
+    {
+
         $kelas2 = Kelas::find($id);
         return view('admin/absensi',['kelas2'=>$kelas2,'user' => Auth::user()]);
     }
-    public function create(Request $request){
-       
-        for($i=0;$i<count($request->siswa);$i++){
-            
-            $check = Absensi::where(['id_siswa' => $request->siswa[$i],'id_kelas' => $request->kelas, 'tanggal' => Carbon::now('Asia/Jakarta')->format('Y-m-d')])->get();
-            if(count($check)==0 && $request->status[$i] != "Hadir"){
-                $absen = new Absensi;
-                $absen->id_siswa = $request->siswa[$i];
-                $absen->id_kelas = $request->kelas;
-                $absen->tanggal = Carbon::now('Asia/Jakarta')->format('Y-m-d');
-                $absen->status = $request->status[$i];
-                $absen->keterangan = $request->status[$i];
-                $absen->save();
 
-            }
-
-
-        }
+    public function create(Request $request)
+    {
+        $this->model = new Absensi;  
+        $this->model->tambah_data($request);
         return redirect('/absensi');
     }
 }
--- app/Models/Absensi.php
+++ app/Models/Absensi.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 use DB;
 
 class Absensi extends Model
@@ -24,10 +25,35 @@ class Absensi extends Model
 
     public function tampil_absen()
     {
-        $data       = DB::table('kelas')
+       $data       = DB::table('kelas')
                     ->join('gurus', 'gurus.id', '=', 'kelas.wali_kelas')
                     ->get(['kelas.nama_kelas','kelas.tingkat_kelas','kelas.kuota','kelas.id','kelas.thn_masuk','kelas.thn_keluar','gurus.nama']);
-        return $data;
+       return $data;
+    }
+
+    public function tambah_data($request)
+    {
+
+        for($i=0;$i<count($request->siswa);$i++){
+            
+            $check = Absensi::where(['id_siswa' => $request->siswa[$i],'id_kelas' => $request->kelas, 'tanggal' => Carbon::now('Asia/Jakarta')
+                                    ->format('Y-m-d')])->get();
+
+            if(count($check) == 0 && $request->status[$i] != "Hadir"){
+                
+                $absen              = new Absensi;
+                $absen->id_siswa    = $request->siswa[$i];
+                $absen->id_kelas    = $request->kelas;
+                $absen->tanggal     = Carbon::now('Asia/Jakarta')->format('Y-m-d');
+                $absen->status      = $request->status[$i];
+                $absen->keterangan  = $request->status[$i];
+                $absen->save();
+
+            }
+
+
+        }
+
     }
 }
 
