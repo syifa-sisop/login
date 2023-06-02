62868e500e116ee42f54861e4e1a91e71f90c72d
Syifa3112 <58512859+syifa-sisop@users.noreply.github.com>
2023-06-02 21:25:30 +0700

    perbaikan siswa

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -14,5 +14,47 @@ class Siswa extends Model
         'jenis_kelamin'
         //'tingkat_kelas'
     ];
+
+    public function kelas()
+    {
+        return $this->belongsToMany('App\Models\Kelas', 'kelas_siswas', 'id_siswa', 'id_kelas');
+    }
+
+    public function search($request)
+    {
+        $siswas = Siswa::where([
+            ['nama', '!=', Null],
+            [function ($query) use ($request) {
+                if (($s = $request->s)) {
+                    $query->orWhere('nama', 'LIKE', '%' . $s . '%')
+                        ->orWhere('nisn', 'LIKE', '%' . $s . '%')
+                        ->orWhere('jenis_kelamin', 'LIKE', '%' . $s . '%')
+                        ->get();
+                }
+            }]
+        ])->paginate(5);
+
+        return $siswas;
+    }
+
+    public function tambah_data($request)
+    {
+
+        $check = Siswa::where(['nisn' => $request->nisn])->get();
+        if($check->count()>0){
+            session()->flash('notif', array('success' => false, 'msgaction' => 'Tambah Data Gagal, Data Telah Ada!'));
+            return redirect()->route('datasiswa');
+        }else{
+            $request->validate([
+            'nama'  => 'required',
+            'nisn'  => 'required',
+            'jenis_kelamin' => 'required'
+            //'tingkat_kelas'  => 'required'
+        ]);
+        // simpan
+        Siswa::create($request->all());
+        }
+
+    }
 }
 
