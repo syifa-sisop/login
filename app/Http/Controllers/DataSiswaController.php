c3691822f1441ace5296c434a09ee102c45428ff
Syifa3112 <58512859+syifa-sisop@users.noreply.github.com>
2023-06-02 21:26:00 +0700

    perbaikan siswa

--- app/Http/Controllers/DataSiswaController.php
+++ app/Http/Controllers/DataSiswaController.php
@@ -10,51 +10,22 @@ class DataSiswaController extends Controller
 {
     public function index(Request $request)
     {
-        $siswa   = Siswa::all();
-        //$siswas  = Siswa::paginate(5);
-        //$guru2 = Siswa::latest()->get();
-        
-
-        $siswas = Siswa::where([
-            ['nama', '!=', Null],
-            [function ($query) use ($request) {
-                if (($s = $request->s)) {
-                    $query->orWhere('nama', 'LIKE', '%' . $s . '%')
-                        ->orWhere('nisn', 'LIKE', '%' . $s . '%')
-                        ->orWhere('jenis_kelamin', 'LIKE', '%' . $s . '%')
-                        ->get();
-                }
-            }]
-        ])->paginate(5);
+      
+        $this->model = new Siswa;
+        $siswas = $this->model->search($request);
 
         return view('admin.siswa')->with([
             'user' => Auth::user(),
             'siswas' => $siswas,
-            'siswa' => $siswa,
+
         ]);
     }
 
     public function store(Request $request)
     {
-        $check = Siswa::where(['nisn' => $request->nisn])->get();
-        if($check->count()>0){
-            session()->flash('notif', array('success' => false, 'msgaction' => 'Tambah Data Gagal, Data Telah Ada!'));
-            return redirect()->route('datasiswa.search');
-        }else{
-            $request->validate([
-            'nama'  => 'required',
-            'nisn'  => 'required',
-            'jenis_kelamin' => 'required'
-            //'tingkat_kelas'  => 'required'
-        ]);
-        // simpan
-        Siswa::create($request->all());
-
-        // redirect
-        return redirect()->route('datasiswa.search')->with('success', 'Data berhasil ditambahkan!');
-        }
-        
-        
+        $this->model = new Siswa;
+        $this->model->tambah_data($request);
+        return redirect()->route('datasiswa')->with('success', 'Data berhasil ditambahkan!');
     }
 
     public function update(Request $request, $id)
@@ -63,7 +34,7 @@ class DataSiswaController extends Controller
         $input  = $request->all();
         $siswa->fill($input)->save();
 
-        return redirect()->route('datasiswa.search')->with('success', 'Data berhasil diupdate!');
+        return redirect()->route('datasiswa')->with('success', 'Data berhasil diupdate!');
     }
 
     public function destroy($id)
@@ -71,7 +42,7 @@ class DataSiswaController extends Controller
         $siswas     = Siswa::find($id);
         $siswas->delete();
 
-        return redirect()->route('datasiswa.search')->with('success', 'Data berhasil dihapus!');
+        return redirect()->route('datasiswa')->with('success', 'Data berhasil dihapus!');
     }
 
 
