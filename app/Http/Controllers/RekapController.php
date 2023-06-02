15a5202b935d09853f033a7f9f0d5e8f96f1f396
Syifa3112 <58512859+syifa-sisop@users.noreply.github.com>
2023-06-02 21:28:58 +0700

    menu rekap

--- app/Http/Controllers/RekapController.php
+++ app/Http/Controllers/RekapController.php
@@ -4,21 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Rekap;
 use App\Models\Kelas;
-use App\Models\Guru;
-use App\Models\KelasSiswa;
-use App\Models\Absensi;
-use DB; 
 
 class RekapController extends Controller
 {
     public function index()
     {
         $kelas2  = Kelas::paginate(5);
-        $data = DB::table('kelas')
-                    ->join('gurus', 'gurus.id', '=', 'kelas.wali_kelas')
-                    ->get(['kelas.nama_kelas','kelas.tingkat_kelas','kelas.kuota','kelas.id','kelas.thn_masuk','kelas.thn_keluar','gurus.nama']);
-        //$guru2 = Kelas::latest()->get();
+
+        $this->model = new Rekap;
+        $data = $this->model->show();
         
         return view('admin.rekap')->with([
             'user'      => Auth::user(),
@@ -30,11 +26,8 @@ class RekapController extends Controller
 
     public function show($id)
     {
-        $data = DB::table('absensis')
-                    ->join('kelas', 'kelas.id', '=', 'absensis.id_kelas')
-                    ->join('siswas', 'siswas.id', '=', 'absensis.id_siswa')
-                    ->where('absensis.id_kelas',$id)
-                    ->get(['absensis.id','siswas.nama','siswas.nisn','siswas.jenis_kelamin', 'absensis.status', 'absensis.tanggal']);
+        $this->model = new Rekap;
+        $data = $this->model->tampil_data($id);
 
         return view('Admin/detail_absen', ['user' => Auth::user(), 'data' =>$data]);
     }
